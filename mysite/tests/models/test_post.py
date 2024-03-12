d37851e9a7e96ba41ab2cb85b7ab997f96f8d9b5
Eduardo Felipe <eduardofelipe_2000@hotmail.com>
2024-03-12 14:41:50 -0300

    django admin

--- mysite/tests/models/test_post.py
+++ mysite/tests/models/test_post.py
@@ -1,6 +1,6 @@
 import pytest
 
-from blog.factories import PostFactory
+from ...blog.factories import PostFactory
 
 
 @pytest.fixture
@@ -8,6 +8,6 @@ def post_published():
     return PostFactory(title='pytest with factory')
 
 
-@pytest.mark.djando_db
+@pytest.mark.django_db
 def test_create_published_post(post_published):
     assert post_published.title == 'pytest with factory'
